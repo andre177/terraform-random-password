85abde24aeb78f1cf3d5858b1e2bdd9c1c306690
Andre Ferreira <andref@peoplesgroup.com>
2024-08-01 16:37:56 -0300

    adding descriptions to the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "bcrypt_hash" {
-  value     = random_password.password.bcrypt_hash
-  sensitive = true
+  value       = random_password.password.bcrypt_hash
+  sensitive   = true
+  description = "A bcrypt hash of the generated random string. NOTE: If the generated random string is greater than 72 bytes in length, `bcrypt_hash` will contain a hash of the first 72 bytes."
 }
 
 output "result" {
-  value     = random_password.password.result
-  sensitive = true
+  value       = random_password.password.result
+  sensitive   = true
+  description = "The generated random string"
 }
